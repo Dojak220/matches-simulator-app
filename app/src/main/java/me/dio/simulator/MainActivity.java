3ab3fd65047a9277e477cfbfb545cfaa3b1696c2
Dorgival Rocha <dojak220@ufrn.edu.br>
2022-08-06 14:30:23 -0300

    Add detail item activity

--- app/src/main/java/me/dio/simulator/MainActivity.java
+++ app/src/main/java/me/dio/simulator/MainActivity.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import me.dio.simulator.databinding.ActivityMainBinding;
+import me.dio.simulator.databinding.MatchItemBinding;
 
 public class MainActivity extends AppCompatActivity {
 
